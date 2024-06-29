4527bd74e92b809c2f695c7f9a6577fa27e4d178
hendri <hendri240xie@gmail.com>
2024-06-29 20:54:12 +0700

    added mahasiswa table

--- db/crud-php.sql
+++ db/crud-php.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jun 25, 2024 at 02:28 AM
+-- Generation Time: Jun 29, 2024 at 01:50 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -40,8 +40,7 @@ CREATE TABLE `barang` (
 --
 
 INSERT INTO `barang` (`id_barang`, `nama`, `jumlah`, `harga`, `tanggal`) VALUES
-(1, 'Keyboard NYK Nemesis', '10', '100000', '2024-06-24 06:24:25'),
-(2, 'Laptop Acer Nitro v15', '5', '10000000', '2024-06-24 06:25:19'),
+(2, 'Laptop Acer Nitro v15', '5', '60000000', '2024-06-24 06:25:19'),
 (3, 'Laptop ASUS TUF F15', '5', '13000000', '2024-06-24 07:08:33'),
 (4, 'Mouse Logitech G102', '10', '130000', '2024-06-24 07:08:33'),
 (5, 'Kalkulator Value 14 Digit', '8', '120000', '2024-06-24 07:08:33'),
@@ -50,7 +49,32 @@ INSERT INTO `barang` (`id_barang`, `nama`, `jumlah`, `harga`, `tanggal`) VALUES
 (9, 'Mouse Gaming Mofi', '10', '89000', '2024-06-24 08:11:09'),
 (10, 'Mesin EDC BNI', '3', '100000', '2024-06-24 08:13:50'),
 (11, 'Kalkulator Casio DR-120R', '2', '1200000', '2024-06-24 08:24:43'),
-(12, 'Jam Tangan Expeditions SM-EXP45', '1', '2500000', '2024-06-24 08:25:08');
+(12, 'Jam Tangan Expeditions SM-EXP45', '1', '2500000', '2024-06-24 08:25:08'),
+(13, 'Game Hades Ori COPYan', '1', '5000', '2024-06-28 06:34:18');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `id_mahasiswa` int NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `prodi` varchar(50) NOT NULL,
+  `jk` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `telepon` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `foto` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id_mahasiswa`, `nama`, `prodi`, `jk`, `telepon`, `email`, `foto`) VALUES
+(1, 'Hendri', 'Sistem Informasi', 'Laki-Laki', '+6282176576399', 'hendrixie45@gmail.com', 'hendri.jpg'),
+(2, 'Hendra', 'Teknik Informatika', 'Teknik Informatika', '081277778888', 'hendra@gmail.com', 'hendra.jpg');
 
 --
 -- Indexes for dumped tables
@@ -63,6 +87,12 @@ ALTER TABLE `barang`
   ADD PRIMARY KEY (`id_barang`);
 
 --
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`id_mahasiswa`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -70,7 +100,13 @@ ALTER TABLE `barang`
 -- AUTO_INCREMENT for table `barang`
 --
 ALTER TABLE `barang`
-  MODIFY `id_barang` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_barang` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `id_mahasiswa` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
